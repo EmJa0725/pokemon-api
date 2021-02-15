78320adaf79c1252cc4b947f9d2bfe5e3cd42f6a
EmJa0725 <67122190+EmJa0725@users.noreply.github.com>
2021-02-15 11:26:18 -0500

    Save pokemon font locally and fixed card mt on Larger Screen

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App';
+import './assets/fonts/Pokemon-Solid.ttf'
 
 
 import "./index.css";
